59c411127cec502f8af3373ee44efc6aa37d69b7
Mina <minallkamel@gmail.com>
2019-08-05 19:18:01 +0200

    remove unused code

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,6 @@
-//use std::process::Command;
-#[macro_use]
+#![allow(dead_code)]
 mod parser;
 
 fn main() {
-  // let output = Command::new("git")
-  //   .arg("status")
-  //   .output()
-  //   .expect("Could not excute");
-  // println!("{}", String::from_utf8_lossy(&output.stdout));
-  // println!("Hello, world!");
-  parser::parse_args();
+  parser::parse_args().unwrap();
 }
